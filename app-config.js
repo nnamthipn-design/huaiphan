ade949c530e53e16bc8f1163f748b81497da11b5
nnamthipn-design <n.namthip.n@gmail.com>
2026-09-02 11:30:43 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbxba2Enfzaz4wu7supLpkW2V-3aQLk4AG6KtPjc6GczcifG4J7wXpcE85pmAZsoKXdBhg/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbyFWD893iv-R0Wf365qNxgo_1tJMqj3XskwQ2YjOxRrj9i4w9HinlXFIIoK5nAuPXUB/exec'
   });
 })();
