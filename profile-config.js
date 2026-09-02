8ed80eff7d15ba0e315de5629c98db77d7c7e35a
nnamthipn-design <n.namthip.n@gmail.com>
2026-09-02 11:44:36 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbzFRWp1gl6q83qbTlmpZ-ZURyAPh8IxyxKpobSbtPLcI1WaBvQ0BGFM2wJgjDRoOMtC/exec';
